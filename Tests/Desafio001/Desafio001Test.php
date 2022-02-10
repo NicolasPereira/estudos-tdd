7c6ae5871bf0e84d83bd4ba7fa1bf17af19219ce
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-02-10 01:23:48 -0300

    feat: create Desafio001Test Class

---
+++ Tests/Desafio001/Desafio001Test.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Desafio001;
+
+use PHPUnit\Framework\TestCase;
+use Root\Www\Desafio001\Desafio001;
+
+class Desafio001Test extends TestCase
+{
+    public function test_should_return_hello_world()
+    {
+        //Arrange
+        $instance = new desafio001();
+        $expected = 'Olá Mundo'.PHP_EOL;
+
+        //Act
+        $result = $instance->execute();
+
+        //Assert
+        $this->assertEquals($result, $expected);
+    }
+}
