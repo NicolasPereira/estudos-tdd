895987905d0d36f0e75119b360d3632086386e92
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-02-10 01:23:41 -0300

    feat: create Desafio001

---
+++ src/Desafio001/Desafio001.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Root\Www\Desafio001;
+
+class Desafio001
+{
+    public function execute(): string
+    {
+        return 'Olá Mundo'.PHP_EOL;
+    }
+}
