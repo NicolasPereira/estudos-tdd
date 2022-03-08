da04cca29af96b3981ab2e71bb9d0599c99c3c3f
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-03-08 03:28:30 -0300

    feat: alterando o fluxo do App para ver o funcionamento

--- src/Desafio002/app/App.php
+++ src/Desafio002/app/App.php
@@ -4,13 +4,18 @@ declare(strict_types=1);
 
 namespace App\Desafio002\app;
 
+use App\Desafio002\app\Host\Host;
 use App\Desafio002\app\User\User;
+use App\Desafio002\app\Host\Services\HostServices;
 
 class App {
 
-    public function index(): string
+    public function index(): User
     {
         $user = new User('nicolas');
-        return $user->name;
+        $hostService = new HostServices();
+        $host = new Host('nicolas', $hostService);
+        $host->makeCoHostUser($user);
+        return $user;
     }
 }
