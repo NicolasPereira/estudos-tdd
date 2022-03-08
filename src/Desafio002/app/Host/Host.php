2d4b6c72aa08c00d0e0979db61d590b2002cd8e2
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-03-08 03:28:47 -0300

    feat: implementando o HostService no host

--- src/Desafio002/app/Host/Host.php
+++ src/Desafio002/app/Host/Host.php
@@ -1,18 +1,22 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Desafio002\app\Host;
 
+use App\Desafio002\app\Host\Services\HostServices;
 use App\Desafio002\app\User\User;
 
 class Host extends User
 {
-    public function __construct(string $name)
-    {
+    public function __construct(
+        string $name, 
+        private HostServices $service
+    ){
         parent::__construct($name, true);
     }
 
     public function makeCoHostUser(User $user): void
     {
-        $user->coHost = true;
+        $this->service->makeCoHostUser($user);
     }
 }
