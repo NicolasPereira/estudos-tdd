63a5802479418bc721be705c40d0f67d693b137f
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-03-13 22:12:03 -0300

    delete: removendo serviço do anfitrião

--- src/Desafio002/app/App.php
+++ src/Desafio002/app/App.php
@@ -6,7 +6,6 @@ namespace App\Desafio002\app;
 
 use App\Desafio002\app\Host\Host;
 use App\Desafio002\app\User\User;
-use App\Desafio002\app\Host\Services\HostServices;
 
 class App {
 
@@ -15,9 +14,6 @@ class App {
         $user = new User('nicolas');
 
         $host = new Host('nicolas');
-        $hostService = new HostServices($host);
-
-        $hostService->makeCoHostUser($user);
         return $user;
     }
 }
