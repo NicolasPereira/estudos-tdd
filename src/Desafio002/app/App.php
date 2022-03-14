5f367fac93a5b355d6384b6f687e67aed172ac0b
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-03-14 01:54:35 -0300

    feat: adicionando comportamento para o App

--- src/Desafio002/app/App.php
+++ src/Desafio002/app/App.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Desafio002\app;
 
+use App\Desafio002\app\ClassRoom\ClassRoom;
 use App\Desafio002\app\Host\Host;
+use App\Desafio002\app\Student\Student;
 use App\Desafio002\app\User\User;
 
 class App {
@@ -12,8 +14,11 @@ class App {
     public function index(): User
     {
         $user = new User('nicolas');
-
+        $student = new Student('nicolas');
         $host = new Host('nicolas');
+        $host->chargeUserHost($student);
+        $arrayUser = [$user, $student];
+        $classRoom = new ClassRoom($arrayUser);
         return $user;
     }
 }
