6e7363f7578f7f6d65e32639ec900c273aace4c0
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-03-14 01:54:03 -0300

    feat: adicionando funcionalidades pro User

--- src/Desafio002/app/User/User.php
+++ src/Desafio002/app/User/User.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace App\Desafio002\app\User;
 
+use App\Desafio002\app\ClassRoom\ClassRoom;
+
 class User
 {
     public function __construct(
@@ -10,4 +12,35 @@ class User
         public bool $coHost = false,
         public bool $microphoneActive = false
     ){}
+
+    public function muteUser(User $user)
+    {
+        if ($this->coHost) {
+            $user->microphoneActive = false;
+        }
+    }
+
+    public function muteAllUser(ClassRoom $room)
+    {
+        if ($this->coHost) {
+            foreach ($room->participantList as $user)
+            {
+                $this->muteUser($user);
+            }
+        }
+    }
+
+    public function addParticipantInClassRoom(User $user, ClassRoom $classRoom)
+    {
+        if (!$this->coHost) {
+            return 'Você não tem permissão para adicionar usuários';
+        }
+
+        if($classRoom->quantityParticipants() > $classRoom::MAX_CAPACITY)
+        {
+            return 'A sala já está lotada';
+        }
+
+        $classRoom->participantList[] = $user;
+    }
 }
